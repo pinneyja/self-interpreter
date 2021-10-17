9602a7892f8d1cfc747cad6228574f1940a47f82
Luke J. McNeil <mcneill1@rose-hulman.edu>
2021-10-17 01:49:48 +0000

    allow for syntactic sugar for binary slots

--- src/parsing/nodes/BinarySlotNode.py
+++ src/parsing/nodes/BinarySlotNode.py
@@ -1,15 +1,17 @@
 from interpreting.objects.SelfSlot import *
+from parsing.nodes.ArgumentSlotNode import *
 from .Node import Node
 
 class BinarySlotNode(Node):
 	def __init__(self, name, expression, arg_name = None):
 		super().__init__()
 		self.name = name
+		if arg_name:
+			expression.slot_list.append(ArgumentSlotNode(arg_name))
 		self.expression = expression
-		self.arg_name = arg_name
 
 	def __str__(self):
-		return "BinarySlot: (name='{}' expression='{}' arg_name='{}')".format(self.name, self.expression, self.arg_name)
+		return "BinarySlot: (name='{}' expression='{}')".format(self.name, self.expression)
 
 	def interpret(self, context):
 		return SelfSlot(self.name, self.expression.interpret(context), True)
--- tests/binary_message_interpreting_test.py
+++ tests/binary_message_interpreting_test.py
@@ -1,15 +1,28 @@
 from parsing.nodes.RegularObjectNode import *
 from parsing.nodes.DataSlotNode import *
+from parsing.nodes.BinarySlotNode import *
 from parsing.nodes.IntegerNode import *
 from parsing.nodes.UnaryMessageNode import *
 from interpreting.Interpreter import *
 
 def test_basic_binary_message_passing():
-	# (| + = (|:arg| 5)|) + 1
+	# (| + = (|:arg| arg)|) + 5
 	interpreter = Interpreter()
 
-	reg_object = RegularObjectNode([ DataSlotNode("+", "=", RegularObjectNode([ArgumentSlotNode("arg")], CodeNode([IntegerNode(5)]))) ])
-	parser_result = CodeNode([BinaryMessageNode(reg_object, "+", IntegerNode(1))])
+	reg_object = RegularObjectNode([ BinarySlotNode("+", RegularObjectNode([ArgumentSlotNode("arg")], CodeNode([UnaryMessageNode(None, "arg")]))) ])
+	parser_result = CodeNode([BinaryMessageNode(reg_object, "+", IntegerNode(5))])
+	interpreted_result = interpreter.interpret(parser_result)
+
+	expected_result = SelfInteger(5)
+
+	assert str(interpreted_result) == str(expected_result)
+
+def test_basic_binary_message_passing_with_syntactic_sugar():
+	# (| + arg = (| | arg)|) + 5
+	interpreter = Interpreter()
+
+	reg_object = RegularObjectNode([ BinarySlotNode("+", RegularObjectNode(code=CodeNode([UnaryMessageNode(None, "arg")])), "arg") ])
+	parser_result = CodeNode([BinaryMessageNode(reg_object, "+", IntegerNode(5))])
 	interpreted_result = interpreter.interpret(parser_result)
 
 	expected_result = SelfInteger(5)
@@ -20,12 +33,30 @@ def test_operators_with_other_meanings():
 	# (| <- = (|:arg| arg)|) <- 5
 	interpreter = Interpreter()
 
-	reg_object = RegularObjectNode([ DataSlotNode("<-", "=", RegularObjectNode([ArgumentSlotNode("arg")], CodeNode([IntegerNode(5)]))) ])
+	reg_object = RegularObjectNode([ DataSlotNode("<-", "=", RegularObjectNode([ArgumentSlotNode("arg")], CodeNode([UnaryMessageNode(None, "arg")]))) ])
 	parser_result = CodeNode([BinaryMessageNode(reg_object, "<-", IntegerNode(5))])
 	interpreted_result_larrow = interpreter.interpret(parser_result)
 
 	# (| = = (|:arg| arg)|) = 5
-	reg_object = RegularObjectNode([ DataSlotNode("=", "=", RegularObjectNode([ArgumentSlotNode("arg")], CodeNode([IntegerNode(5)]))) ])
+	reg_object = RegularObjectNode([ DataSlotNode("=", "=", RegularObjectNode([ArgumentSlotNode("arg")], CodeNode([UnaryMessageNode(None, "arg")]))) ])
+	parser_result = CodeNode([BinaryMessageNode(reg_object, "=", IntegerNode(5))])
+	interpreted_result_equal = interpreter.interpret(parser_result)
+
+	expected_result = SelfInteger(5)
+
+	assert str(interpreted_result_larrow) == str(expected_result)
+	assert str(interpreted_result_equal) == str(expected_result)
+
+def test_operators_with_other_meanings_syntactic_sugar():
+	# (| <- arg = (| | arg)|) <- 5
+	interpreter = Interpreter()
+
+	reg_object = RegularObjectNode([ BinarySlotNode("<-", RegularObjectNode(code=CodeNode([UnaryMessageNode(None, "arg")])), "arg") ])
+	parser_result = CodeNode([BinaryMessageNode(reg_object, "<-", IntegerNode(5))])
+	interpreted_result_larrow = interpreter.interpret(parser_result)
+
+	# (| = arg = (| | arg)|) = 5
+	reg_object = RegularObjectNode([ BinarySlotNode("=", RegularObjectNode(code=CodeNode([UnaryMessageNode(None, "arg")])), "arg") ])
 	parser_result = CodeNode([BinaryMessageNode(reg_object, "=", IntegerNode(5))])
 	interpreted_result_equal = interpreter.interpret(parser_result)
 	
