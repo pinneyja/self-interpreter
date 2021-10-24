8f7a794ef9de0c02e2962d53dd250142e14adf3e
Luke J. McNeil <mcneill1@rose-hulman.edu>
2021-10-24 03:20:51 +0000

    Lobby

--- src/interpreting/Interpreter.py
+++ src/interpreting/Interpreter.py
@@ -2,11 +2,15 @@ from parsing.Parser import *
 from interpreting.objects.SelfException import *
 from parsing.SelfParsingError import *
 from Messages import *
+from interpreting.objects.SelfLobby import *
 
 class Interpreter:
+	def __init__(self):
+		self.lobby = SelfLobby()
+
 	def interpret(self, syntaxTree):
 		try:
-			result = syntaxTree.interpret(None)
+			result = syntaxTree.interpret(self.lobby)
 		except (SelfException, SelfParsingError) as e:
 			raise e
 		except Exception as e:
